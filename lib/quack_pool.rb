1dbb1b962d06f0f6719d2e1c12d030833fe3a2fb
Rob <mail@robfors.com>
2018-04-13 02:56:40 -0400

    Reorder mehtods

--- lib/quack_pool.rb
+++ lib/quack_pool.rb
@@ -11,6 +11,16 @@ class QuackPool
     @condition_variable = ConditionVariable.new
   end
   
+  def absorb_resource(resource)
+    @mutex.synchronize do
+      raise ArgumentError, "'resource' does not belong to this pool" unless @resources.include?(resource)
+      raise ArgumentError, "'resource' already in pool" if @available_resources.include?(resource)
+      @available_resources.push(resource)
+      @condition_variable.signal
+    end
+    nil
+  end
+  
   def release_resource
     @mutex.synchronize do
       if @available_resources.any?
@@ -23,27 +33,17 @@ class QuackPool
     end
   end
   
-  def absorb_resource(resource)
-    @mutex.synchronize do
-      raise ArgumentError, "'resource' does not belong to this pool" unless @resources.include?(resource)
-      raise ArgumentError, "'resource' already in pool" if @available_resources.include?(resource)
-      @available_resources.push(resource)
-      @condition_variable.signal
-    end
-    nil
-  end
-  
   private
-  
-  def release_available_resource
-    @available_resources.pop
-  end
   
   def build_new_resource
     new_resource = @resource_class.new
     raise "'new_resource' must be unique" if @resources.include?(new_resource)
     @resources << new_resource
     new_resource
+  end
+  
+  def release_available_resource
+    @available_resources.pop
   end
   
   def release_next_available_resource
--- quack_pool.gemspec
+++ quack_pool.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'quack_pool'
-  s.version     = '0.0.0'
-  s.date        = '2018-04-03'
+  s.version     = '0.0.1'
+  s.date        = '2018-04-13'
   s.summary     = 'A simple resource pool'
   s.description = "A simple resource pool that accepts a resource class to build the pool's resources from."
   s.authors     = 'Rob Fors'
